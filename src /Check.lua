9ef20231e2c945563385a9e99a18a6a361482fa1
christian <69432633+Aegians@users.noreply.github.com>
2023-09-06 22:23:02 -0400

    Update Check.lua

--- src /Check.lua
+++ src /Check.lua
@@ -1,10 +1,10 @@
 local game = game
 local select, pcall, loadstring , warn = select, pcall, loadstring, warn
 
-local Success, ESP = pcall(select(2, pcall(loadstring, game:HttpGet("https://raw.githubusercontent.com/Aegians/AxisHub/main/esp.lua"))))
+local Success, ESP = pcall(select(2, pcall(loadstring, game:HttpGet("https://raw.githubusercontent.com/Aegians/AxisHub/main/src%20/ESP.lua"))))
 
 if not Success then
-    Success, ESP = pcall(select(2, pcall(loadstring, game:HttpGet("https://raw.githubusercontent.com/Aegians/AxisHub/main/uwp%20settings.lua"))))
+    Success, ESP = pcall(select(2, pcall(loadstring, game:HttpGet("https://raw.githubusercontent.com/Aegians/AxisHub/main/src%20/UWP%20Module.lua"))))
 
     if not Success then
         return warn("AEGIANS_ESP > Loader - Your script execution software does not support this module.")
